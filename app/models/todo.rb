16e78d0cbc6390533359b0c7c9605ddda26bfa22
zs <baloghzsof@gmail.com>
2019-05-03 05:27:02 +0200

    Todo model validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Todo < ApplicationRecord
+  validates :title, presence: true
+  before_validation :handle_completed_at
+
+  private
+
+  def handle_completed_at
+    if completed?
+      self.completed_at ||= Time.zone.now
+    else
+      self.completed_at = nil
+    end
+  end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,5 +3,49 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveSupport::Testing::TimeHelpers
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+  end
+
+  describe 'completed_at' do
+    context 'mark a todo completed' do
+      context 'todo is completed' do
+        subject { build(:todo, :completed) }
+
+        it 'will not change completed_at' do
+          expect { subject.update(completed: true) }.not_to(change { subject.completed_at })
+        end
+      end
+
+      context 'todo is uncompleted' do
+        subject { build(:todo, :uncompleted) }
+
+        it 'will set completed_at to now' do
+          freeze_time do
+            expect { subject.update(completed: true) }.to change { subject.completed_at }.to(Time.current)
+          end
+        end
+      end
+    end
+
+    context 'mark a todo uncompleted' do
+      context 'todo is completed' do
+        subject { build(:todo, :completed) }
+
+        it 'will set completed_at to nil' do
+          expect { subject.update(completed: false) }.to change { subject.completed_at }.to(nil)
+        end
+      end
+
+      context 'todo is uncompleted' do
+        subject { build(:todo, :uncompleted) }
+
+        it 'will not change completed_at' do
+          expect { subject.update(completed: false) }.not_to(change { subject.completed_at })
+        end
+      end
+    end
+  end
 end
